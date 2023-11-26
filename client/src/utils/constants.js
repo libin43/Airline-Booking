bfd03f379755936c263d79ec49e1a986af4bf31c
Libin Biji <libinbiji43@gmail.com>
2023-11-26 09:09:23 +0530

    update: baseUrl

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,7 @@
 let BASE_URL;
 
 if(import.meta.env.MODE === 'production'){
-    BASE_URL = ''
+    BASE_URL = 'https://airline-server.onrender.com/api/v1'
 }
 else{
     BASE_URL = 'http://localhost:7000/api/v1'
