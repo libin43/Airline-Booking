73441b239642cc5d451a548cfe373e62eb0a4c2a
Libin Biji <libinbiji43@gmail.com>
2023-11-26 09:33:04 +0530

    update: inputField

--- client/src/components/Input/InputField.tsx
+++ client/src/components/Input/InputField.tsx
@@ -3,14 +3,15 @@ import { AiTwotoneEyeInvisible } from "react-icons/ai";
 
 export const InputField = ({name, type, value, style, onChange }) => {
     const [hidePassword, setHidePassword] = useState(true)
-    const handleVisibility = () => {
-    }
+
+    const inputType = hidePassword ? type : 'text';
+
   return (
     <>
     <div className={`${type === 'password'? 'flex justify-center xl:mx-[1.4%]  w-[100%]': ''} input-container text-center`}>
         <input
         className={style}
-        type={ type ==='password' && hidePassword? 'password': type}
+        type={inputType}
         name={name}
         value={value}
         onChange={(e)=> onChange(e.target.value, name)}
